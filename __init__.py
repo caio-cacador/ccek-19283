ce155d78f5a0072ad3829ecbfa44f44b257c66f3
Caio <caiosilva@wareline.com.br>
2019-09-04 13:48:23 -0300

    balanceamento de carga

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 # /usr/bin/python3
-import optparse, os
+import optparse, sys
 from servidor import Servidor
 from usuario import Usuario
 
 
 def encerra_aplicacao(mensagem):
     print(mensagem)
-    os.abort()
+    sys.exit(0)
 
 
 def le_input(input_txt):
@@ -16,17 +16,19 @@ def le_input(input_txt):
             linha = file.read().replace('\n', '')
             for texto in linha:
                 if len(texto) > 1:
-                    encerra_aplicacao("[-] Arquivo invalido, deve haver um caracter por linha!")
+                    encerra_aplicacao("[-] Arquivo inválido, deve haver um caracter por linha.")
 
                 linhas.append(int(texto))
 
     except ValueError:
-        encerra_aplicacao("[-] No arquivo deve contas apenas numeros.")
-    except FileNotFoundError as ex:
-        encerra_aplicacao("[-] Arquivo input não encontrado.")
+        encerra_aplicacao("[-] No arquivo deve conter apenas números inteiros.")
+    except FileNotFoundError:
+        encerra_aplicacao("[-] Arquivo input não encontrado, verifique se o caminho esta correto.")
+    except IsADirectoryError:
+        encerra_aplicacao("[-] Arquivo input não encontrado, verifique se o caminho esta correto.")
 
     if len(linhas) <= 2:
-        encerra_aplicacao('[-] Arquivo invalido, não contêm o número de novos usuários para cada tick')
+        encerra_aplicacao('[-] Arquivo inválido, não contêm o número de novos usuários para cada tick')
 
     # primeira linha: valor de ttask  |  1 ≤ ttask ≤ 10
     if not 1 <= linhas[0] <= 10:
@@ -42,74 +44,116 @@ def le_input(input_txt):
 def get_argumentos():
     parser = optparse.OptionParser(description="Calcula backend servidor.")
     parser.add_option("-i", "--input", dest="input", default="input.txt",
-                      help="Caminho do arquivo input (arquivo com os dados a serem lidos), ex: /home/caio/input.txt")
+                      help="Caminho do arquivo input (arquivo com os dados a serem lidos), ex: /home/user/input.txt")
     parser.add_option("-o", "--output", dest="output", default="output.txt",
-                      help="Caminho do arquivo output (arquivo onde será salvo os resultados), ex: /home/caio/output.txt")
+                      help="Caminho do arquivo output (arquivo onde será salvo os resultados), ex: /home/user/output.txt")
 
     (options, arguments) = parser.parse_args()
     return options
 
 
-def removeServidoresVazio(__servidores):
-    return [servidor for servidor in __servidores if not servidor.is_empty()]
+# atualiza a lista de servidores removendo os vazios
+def removeServidorVazio(__servidores):
+    return [__servidor for __servidor in __servidores if not __servidor.is_empty()]
 
 
+# escolhe o melhor servidor para colocar o usuário
 def getServidor(__servidores):
     melhor_servidor = None
-    for servidor in __servidores:
-        if not servidor.is_full():
+    for __servidor in __servidores:
+        if not __servidor.is_full():
+            """
+            escolhe o servidor mais cheio para que os demais possam ser
+            encerrados o quanto antes
+            """
             if not melhor_servidor:
-                melhor_servidor = servidor
-            elif servidor.getTotalTtask() > melhor_servidor.getTotalTtask():
-                melhor_servidor = servidor
+                melhor_servidor = __servidor
+            elif __servidor.getTotalTtask() > melhor_servidor.getTotalTtask():
+                melhor_servidor = __servidor
 
     if melhor_servidor:
         return melhor_servidor
 
+    # cria um servidor novo se não houver nenhum disponível
     new_servidor = Servidor(umax)
     __servidores.append(new_servidor)
     return new_servidor
 
 
-def atualizaServidores(__servidores):
-    status = ''
-    for servidor in __servidores:
-        servidor.usuarios = [_user for _user in servidor.usuarios if not _user.finished()]
-        servidor.update()
-        if len(servidor.usuarios) > 0:
-            if status != '':
-                status += ','
-            status += str(len(servidor.usuarios))
+# atualiza todos os servidores
+def atualizaServidores(__servidores, __output):
+    qtd_usuarios = ''
+    for __servidor in __servidores:
+        # atualiza a lista de usuarios removendo os que já encerraram o numero de ttask
+        __servidor.usuarios = [_user for _user in __servidor.usuarios if not _user.finished()]
 
-    print(status)
+        # atualiza o servidor (atualiza o custo do servidor e a ttask do usuario)
+        __servidor.update()
+
+        # pega a quantidade de usuarios no servidor para gravar no output.txt
+        if len(__servidor.usuarios) > 0:
+            if qtd_usuarios != '':
+                qtd_usuarios += ','
+            qtd_usuarios += str(len(__servidor.usuarios))
+
+    # adiciona a quantidade de usuário de cada servidor no output.txt
+    if qtd_usuarios != '':
+        __output.append(qtd_usuarios)
 
 
-if __name__ == '__main__':
-    argumento = get_argumentos()
-    input_txt = argumento.input
-    output_txt = argumento.output
-    ttask, umax, inputs = le_input(input_txt)
+def start():
+    print('[+] Iniciando.')
 
     servidores = []
     custo_total = 0
     output = []
 
-    for number in inputs:
-        for i in range(int(number)):
+    # percorre as linhas que contem o número de novos usuários
+    for numero in inputs:
+        for i in range(int(numero)):
+            # cria usuário por usuário e o coloca no melhor servidor
             getServidor(servidores).usuarios.append(Usuario(ttask))
 
-
-        atualizaServidores(servidores)
-        servidores = removeServidoresVazio(servidores)
+        atualizaServidores(servidores, output)
+        servidores = removeServidorVazio(servidores)
         custo_total += len(servidores)
 
+    # continua até que todos os servidores sejam finalizados.
     while len(servidores) > 0:
-        atualizaServidores(servidores)
-        servidores = removeServidoresVazio(servidores)
+        atualizaServidores(servidores, output)
+        servidores = removeServidorVazio(servidores)
         custo_total += len(servidores)
 
+    # acrescenta o ultimo 0, pois não há mais servidores
+    output.append(str(len(servidores)))
+
+    # grava o custo total
     output.append(str(custo_total))
-    with open(output_txt, 'w') as file:
-        for i in output:
-            file.writelines(i)
+
+    # escreve o output.txt
+    try:
+        with open(output_txt_path, 'w') as file:
+            for i in output:
+                file.write(i + '\n')
+    except FileNotFoundError:
+        encerra_aplicacao("[-] Arquivo output não encontrado, verifique se o caminho esta correto.")
+    except IsADirectoryError:
+        encerra_aplicacao("[-] Arquivo output não encontrado, verifique se o caminho esta correto.")
+
     print('[-] Custo Total: ', custo_total)
+    print('[-] Salvo Em: ', output_txt_path)
+
+
+if __name__ == '__main__':
+    """
+    recebe os argumentos
+    se não for passado nenhum argumento é pego o 'default'
+    """
+    argumentos = get_argumentos()
+    input_txt_path = argumentos.input
+    output_txt_path = argumentos.output
+
+    # recebe os valores do input.txt
+    ttask, umax, inputs = le_input(input_txt_path)
+
+    start()
